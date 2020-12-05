d9a4e2e8b798dd664a5dbb7ac67030a453183c72
pontakornth <pontakorn61@gmail.com>
2020-12-05 11:38:31 +0700

    feat: Display filename instead of full path

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use std::fs;
+
+fn repeat_char(ch: char, n: i32) -> String {
+    let mut result = String::new();
+    for _ in 0..n {
+        result.push(ch);
+    }
+    result
+}
+
 fn main() {
     let paths = fs::read_dir("./").unwrap();
     for path in paths {
-        println!("{}", path.unwrap().path().display());
+        println!("{} {}", repeat_char('-', 3), path.unwrap().file_name().into_string().unwrap());
     }
     println!("Hello, world!");
 }

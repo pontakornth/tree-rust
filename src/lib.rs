a91032fb37d923f695400f3bb6098971100b9845
pontakornth <pontakorn61@gmail.com>
2020-12-05 17:49:46 +0700

    fix: Forgetting print main

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use std::fs;
+
+pub fn repeat_char(ch: char, n: i32) -> String {
+    let mut result = String::new();
+    for _ in 0..n {
+        result.push(ch);
+    }
+    result
+}
+
+
+pub fn print_dir(path: &str, padding: i32) {
+    let inner_paths = fs::read_dir(path).unwrap();
+    for path in inner_paths {
+        let entry = path.unwrap();
+        let file_name = entry.file_name().into_string().unwrap();
+        let file_type = entry.file_type().unwrap();
+        println!("{} {}", repeat_char('-', padding), file_name);
+        if file_type.is_dir() {
+            let file_path = entry.path();
+            print_dir(file_path.to_str().unwrap(), padding + 2);
+        } 
+    
+
+    }
+}

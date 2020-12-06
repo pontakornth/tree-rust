148f431c33494548872013f2a96b3ee130c59ea3
pontakornth <pontakorn61@gmail.com>
2020-12-06 12:03:01 +0700

    feat: Implement other directory tree

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use tree_rust::{print_dir};
+use clap::{Arg, App};
 
 
 fn main() {
-    print_dir("./", 2);
+    let matches = App::new("TreeRust")
+                     .version("0.1")
+                     .author("Pontakorn Paesaeng <pontakorn61@gmail.com>")
+                     .arg(Arg::with_name("path")
+                         .short("p")
+                         .index(1)
+                         .default_value("./")
+                     )
+                     .get_matches();
+    let path = matches.value_of("path").unwrap();
+    print_dir(path, 2);
 }
